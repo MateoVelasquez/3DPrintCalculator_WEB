694cbf7bfa08fa03ab9913e8380c3b62647c8e28
MateoVelasquez <mvelasquez@sier.com.co>
2022-06-15 12:56:28 -0500

    modificados modelos de tabla

--- app/models.py
+++ app/models.py
@@ -6,16 +6,10 @@ Módulo que contiene los modelos en base de datos.
 from . import db
 
 
-class CfgProfile(db.Model):
-    __tablename__ = 'cfg_profile'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True, nullable=False)
-
-
 class CfgParameters(db.Model):
     __tablename__ = 'cfg_parameters'
-    id = db.Column(db.Integer, primary_key=True)
-    profile  = db.Column(db.Integer, db.ForeignKey('cfg_profile.id'))
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    profile_name = db.Column(db.String(255), unique=True, nullable=False)
     material_cost = db.Column(db.Integer, nullable=False)
     material_density = db.Column(db.Float, nullable=False)
     spool_weigth = db.Column(db.Integer, nullable=False)
@@ -28,11 +22,20 @@ class CfgParameters(db.Model):
     workforce_cost = db.Column(db.Float, nullable=False)
     measure_unit = db.Column(db.Boolean, nullable=False)
 
-    relprof = db.relationship('CfgProfile')
 
     def to_json(self):
-        pass
-
-
-
-
+        return {
+            'id': self.id,
+            'profile_name': self.profile_name,
+            'material_cost': self.material_cost,
+            'material_density': self.material_density,
+            'spool_weigth' : self.spool_weigth,
+            'electric_cost': self.electric_cost,
+            'printer_power': self.printer_power,
+            'printer_price': self.printer_price,
+            'amortization_years': self.amortization_years,
+            'daily_printer_time' : self.daily_printer_time,
+            'failure_rate_prcent': self.failure_rate_prcent,
+            'workforce_cost': self.workforce_cost,
+            'measure_unit': self.measure_unit
+        }
